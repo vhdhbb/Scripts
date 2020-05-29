f953fe7d8757ceadab487404f91e116ee46782f9
vahid habibi <vhdhbb@yahoo.com>
2020-05-29 10:03:16 +0930

    the first change in sc_image.sh

--- sc_image.sh
+++ sc_image.sh
@@ -2,3 +2,13 @@
 
 echo "changing name pictures...."
 echo "we do it later ..."
+MYPATH="/home/vhdhbb/Videos/"
+cd $MYPATH
+if [[ -d /var/log ]]; then
+    echo "this is a directory "
+    ls /var/log
+else
+    echo " it is not a directory"
+fi
+sleep 10 
+clear
